49dd5bd68fac76b30d5fd1b3bc87d51729ab7cbd
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-11-20 00:44:25 +0530

    Added some models and basic scripts

---
+++ Shrinking Worlds/Assets/Scripts/FauxGravityAttractor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityAttractor : MonoBehaviour
+{
+    public static FauxGravityAttractor instance;
+    private SphereCollider col;
+
+    private void Awake() {
+        instance = this;
+        col = instance.GetComponent<SphereCollider>();
+    }
+
+    // public varaible
+    public float gravity = -2f;
+
+    public void PlaceOnSurface(Rigidbody bodyRB) {
+        bodyRB.MovePosition((bodyRB.position - this.transform.position).normalized * (transform.localScale.x * col.radius));
+    }
+
+    public void Attract(Transform body) 
+    {
+        Vector3 gravityUp = (body.position - this.transform.position).normalized;
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation(body.up, gravityUp) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 1 * Time.deltaTime);
+    }
+}
---
+++ Shrinking Worlds/Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FauxGravityBody : MonoBehaviour
+{
+    // public varailables
+    private FauxGravityAttractor fauxGravityAttractor;
+    public bool placeOnSurface;
+
+    // private varaibles
+    private Transform myTransform;
+    private Rigidbody rb;
+
+    
+    /// <summary>
+    // Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        // get the only instance of attractor
+        fauxGravityAttractor = FauxGravityAttractor.instance;
+
+        // setup the rigidbody consraints
+        rb = this.GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb.useGravity = false;        
+
+        // asign transform of this object to varaible
+        myTransform = this.transform;
+
+        // place the player on surface
+        if(placeOnSurface) {
+            fauxGravityAttractor.PlaceOnSurface(rb);
+        }
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        fauxGravityAttractor.Attract(myTransform);        
+    }
+}
