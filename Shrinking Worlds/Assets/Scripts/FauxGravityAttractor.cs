257ae698132e080350e9cb85eae7007898756188
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-11-21 11:46:14 +0530

    Implemented shrinking for the planet

--- Shrinking Worlds/Assets/Scripts/FauxGravityAttractor.cs
+++ Shrinking Worlds/Assets/Scripts/FauxGravityAttractor.cs
@@ -13,7 +13,7 @@ public class FauxGravityAttractor : MonoBehaviour
     }
 
     // public varaible
-    public float gravity = -2f;
+    public float gravity = -10f;
 
     public void PlaceOnSurface(Rigidbody bodyRB) {
         bodyRB.MovePosition((bodyRB.position - this.transform.position).normalized * (transform.localScale.x * col.radius));
@@ -23,10 +23,9 @@ public class FauxGravityAttractor : MonoBehaviour
     {
         Vector3 gravityUp = (body.position - this.transform.position).normalized;
 
-        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+        body.AddForce(gravityUp * gravity);
 
         Quaternion targetRotation = Quaternion.FromToRotation(body.transform.up, gravityUp) * body.rotation;
-        // body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 15 * Time.deltaTime);
         body.MoveRotation(targetRotation);
     }
 }
---
+++ Shrinking Worlds/Assets/Scripts/ShrinkPlanet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkPlanet : MonoBehaviour
+{
+    // public variables
+
+    [Range(.01f, .05f)]
+    public float shrinkSpeed;
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        this.transform.localScale *= 1f - (shrinkSpeed * Time.fixedDeltaTime);
+        Debug.Log(this.transform.localScale);
+    }
+}
