cfc3c3a40c15f44b2a7a0ea314fee2eec82ccffe
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-11-22 10:37:19 +0530

    Added destroy on collision for car

---
+++ Shrinking Worlds/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // puble varaibles
+    public GameObject deathEffect;
+
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.tag == "Meteror") {
+            Instantiate(deathEffect, this.transform.position, this.transform.rotation);
+
+            PlayerController movement = this.GetComponent<PlayerController>();
+
+            Destroy(this.gameObject);
+        }
+    }
+}
