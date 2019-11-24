754e04333e756b8ad1732dfeb334ef3b7349d07d
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-11-24 16:05:19 +0530

    Added somefeatures

---
+++ Shrinking Worlds/Assets/Scripts/StarLightFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarLightFollow : MonoBehaviour
+{
+    // public variables
+    public GameObject target;
+
+    // private variables
+    private Light directionLight;
+    private Quaternion targetRotation;
+
+    private void Start()
+    {
+        directionLight = this.GetComponentInChildren<Light>();
+    }
+
+    private void FixedUpdate() 
+    {
+        this.transform.LookAt(target.transform);
+    }
+}
