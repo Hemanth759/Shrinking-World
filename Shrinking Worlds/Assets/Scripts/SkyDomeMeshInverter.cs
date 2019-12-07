13aae4308d636ea8e636e8d3c6cf564b450607fb
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-12-07 13:30:02 +0530

    Added star and inverse sky script

---
+++ Shrinking Worlds/Assets/Scripts/SkyDomeMeshInverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class SkyDomeMeshInverter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+
+        for(int i=0;i<mesh.subMeshCount;i++) {
+            int[] tris = mesh.GetTriangles(i);
+            for(int j=0;j<tris.Length;j+=3) {
+                // swap the order of tri-verters
+                int temp = tris[j];
+                tris[j] = tris[j+1];
+                tris[j+1] = temp;
+            }
+
+            mesh.SetTriangles(tris, i);
+        }
+
+        // mesh.RecalculateNormals();
+    }
+}
