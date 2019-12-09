41a1a013d264247247797dab7b0aaac0def51d2f
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-12-09 23:09:49 +0530

    Removed useless imports

--- Shrinking Worlds/Assets/Scripts/GameManager.cs
+++ Shrinking Worlds/Assets/Scripts/GameManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
