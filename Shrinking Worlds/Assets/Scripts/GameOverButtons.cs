3c0b49f4436a304ecea9c1b50ab86ef8224f4741
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-12-09 19:46:14 +0530

    Modified few things

--- Shrinking Worlds/Assets/Scripts/GameOverButtons.cs
+++ Shrinking Worlds/Assets/Scripts/GameOverButtons.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
 public class GameOverButtons : MonoBehaviour
 {
+    // private variables
+    private AudioSource audioSource;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
     public void onClick() {
         switch (this.gameObject.name)
         {
             case "RetryButton":
+                audioSource.Play();
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
                 break;
             case "QuitButton":
+                audioSource.Play();
                 Application.Quit();
                 break;
             default:
